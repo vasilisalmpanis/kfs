4b1b5e2486a6d28b3e1121c9cb5e814aa2bca3f7
Polina Simonenko <rabarbrablad@gmail.com>
2025-01-04 16:48:38 +0100

    feat: tty new lines

--- src/kernel/tty.zig
+++ src/kernel/tty.zig
@@ -13,12 +13,34 @@ pub const TTY = struct {
         };
     }
 
-    fn _scroll() void {}
+    fn _scroll(self: *TTY) void {
+        self._y = 0;
+    }
 
-    pub fn print(self: TTY, msg: [*:0] const u8, len: usize) void {
+    fn printChar(self: *TTY, c: u8) void {
+        if (c == '\n') {
+            self._y += 1;
+            self._x = 0;
+            if (self._y >= self.height) {
+                self._scroll();
+            }
+            return ;
+        }
+        self._vga[self._y * self.width + self._x] = (0x0f << 8) | @as(u16, c);
+        self._x += 1;
+        if (self._x >= self.width) {
+            self._x = 0;
+            self._y += 1;
+        }
+        if (self._y >= self.height) {
+            self._scroll();
+        }
+    }
+
+    pub fn print(self: *TTY, msg: [*:0] const u8) void {
         var idx: usize = 0;
-        while (idx < len ) : (idx += 1)
-            self._vga[idx] = (0x0f << 8) | @as(u16, msg[idx]);
+        while (msg[idx] != 0) : (idx += 1)
+            self.printChar(msg[idx]);
     }
 
     pub fn setColor() void {}
