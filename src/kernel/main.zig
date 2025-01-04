66259a23171ce2f144095244987c86c4b9bc57fd
Polina Simonenko <rabarbrablad@gmail.com>
2025-01-04 16:48:38 +0100

    feat: tty new lines

--- src/kernel/main.zig
+++ src/kernel/main.zig
@@ -1,7 +1,7 @@
 const TTY = @import("tty.zig").TTY;
 
 export fn kernel_main() noreturn {
-    const tty = TTY.init(80, 25);
-    tty.print("42", 3);
+    var tty = TTY.init(80, 25);
+    tty.print("42\n");
     while (true) {}
 }
