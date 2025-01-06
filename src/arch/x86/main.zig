bb2222425fc874e5fc0261f38b729c35fbc9a088
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2025-01-06 18:28:04 +0100

    kfs: arch: platform dependent code library
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

---
+++ src/arch/x86/main.zig
@@ -0,0 +1,2 @@
+pub const io = @import("io.zig");
+pub const system = @import("system/system.zig");
---
+++ src/arch/x86/system/system.zig
@@ -0,0 +1,21 @@
+const io = @import("../io.zig");
+
+pub fn reboot() noreturn {
+    var temp : u8 = io.inb(0x64);
+
+    asm volatile ("cli"); // disable interrupts
+
+    while ((temp & (1 << 1)) != 0) {
+        temp = io.inb(0x64); // empty user data
+        if ((temp & (1 << 0)) != 0) {
+            _ = io.inb(0x60); // empty keyboard data
+        }
+    }
+    _ = io.outb(0x64, 0xFE); // CPU reset
+    while (true) {
+        asm volatile ("hlt");
+        // in case of non maskable interrupts
+        // halt again.
+    }
+}
+
