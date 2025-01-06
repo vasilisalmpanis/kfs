ff5f99e5423bb60eae4db1971516f3d8a5eb7902
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2025-01-06 20:40:22 +0100

    kfs: multiple ttys and cursor
    add 10 default ttys and simple shortcuts
    to switch between them. Handles colour and
    adds cursor.
    
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- src/kernel/main.zig
+++ src/kernel/main.zig
@@ -4,34 +4,17 @@ const gdt = @import("gdt.zig");
 const kbd = @import("drivers").kbd;
 const io = @import("arch").io;
 const system = @import("arch").system;
+const screen = @import("screen.zig");
 
 
 export fn kernel_main() noreturn {
-    var tty = TTY.TTY.init(80, 25);
-    var color: u8 = TTY.vga_entry_color(TTY.ConsoleColors.Red, TTY.ConsoleColors.Black);
-    TTY.current_tty = &tty;
-    // tty.print("42\nsecond line\n\t\tjust some tabs\nnewlines!!!!\n\n\n\n.\n", color);
-    // color = TTY.vga_entry_color(TTY.ConsoleColors.Blue, TTY.ConsoleColors.Black);
-    // tty.print("42\nsecond line\n\t\tjust some tabs\nnewlines!!!!\n\n\n\n.\n", color);
-    // tty.print("42\nsecond line\n\t\tjust some tabs\nnewlines!!!!\n\n\n\n.\n", null);
-    // tty.print("1\n2\n3\n4\n5\n6\n7\n8\n9", null);
-    // inline for (@typeInfo(TTY.ConsoleColors).Enum.fields) |f| {
-    //     const clr: u8 = TTY.vga_entry_color(@field(TTY.ConsoleColors, f.name), TTY.ConsoleColors.Black);
-    //     tty.print("Hello World in all the colors\n", clr);
-    // }
-    // const one_plus_one: i32 = 1 + 1;
-    // const string = "this is a string";
-    // TTY.printf("Hello World! {d} {s}\n", .{ one_plus_one, string });
-    // tty._x = 0;
-    // tty._y -= 1;
-    // tty.print("test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test ", null);
-    color = TTY.vga_entry_color(TTY.ConsoleColors.Blue, TTY.ConsoleColors.Black);
-    tty.print("STARTING..\n", color);
-    // gdt.init_gdt();
-    // tty.print("GDT INITIALIZED..\n", color);
-    // interrupts.idt_init();
-    // tty.print("IDT INITIALIZED..\n", color);
-    // TTY.printf("keycode {}\n", .{byte});
+    var scrn : screen.Screen = screen.Screen.init();
+    _ = &scrn;
+    TTY.current_tty = &scrn.tty[0];
+    inline for (@typeInfo(TTY.ConsoleColors).Enum.fields) |f| {
+        const clr: u8 = TTY.vga_entry_color(@field(TTY.ConsoleColors, f.name), TTY.ConsoleColors.Black);
+        TTY.current_tty.?.print("42\n", clr);
+    }
     var last_scancode: u8 = 0;
     var key_pressed: bool = false;
 
@@ -45,7 +28,12 @@ export fn kernel_main() noreturn {
                     TTY.current_tty.?.remove();
                 } else if ( byte == 0x3A) { // CAPSLOCK to reboot (random number)
                     system.reboot();
-                } else TTY.printf("{s}", .{key});
+                } else if (byte == 0x1D) {
+                    TTY.current_tty.?.move(byte);
+                } else if (byte >= 0x01 and byte < 0x0A) {
+                    TTY.current_tty = &scrn.tty[byte - 2];
+                    TTY.current_tty.?.render();
+                } else TTY.current_tty.?.print(key, null);
             }
             key_pressed = true;
         } else if (key_pressed) {
