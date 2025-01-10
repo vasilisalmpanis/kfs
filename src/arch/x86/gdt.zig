58d1edface9ff0caa9c076d7a7ad438e701b010f
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2025-01-10 08:33:51 +0100

    kfs: gdt correct base for gdt
    we should store the table at address 0x00000800
    according to the subject. this change using ptrFromInt
    and num as an index to do exactly that.
    
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- src/arch/x86/gdt.zig
+++ src/arch/x86/gdt.zig
@@ -16,17 +16,19 @@ const gdt_entry = packed struct {
 
 var gdt_ptr : gdtr = undefined;
 
-var gdt_entries: [5]gdt_entry = undefined;
+// var gdt_entries: [5]gdt_entry = undefined;
 pub fn gdt_set_entry(num: u32, base: u32, limit: u32, access: u8, gran: u8) void {
-   gdt_entries[num].base_low = @truncate(base & 0xFFFF);
-   gdt_entries[num].base_middle  = @truncate((base >> 16) & 0xFF);
-   gdt_entries[num].base_high = @truncate((base >> 24) & 0xFF);
+    // load the entry using num as an index into GDTBASE (0x00000800)
+    var gdt_temp: *gdt_entry = @ptrFromInt(GDTBASE + (num * @sizeOf(gdt_entry)));
+    gdt_temp.base_low = @truncate(base & 0xFFFF);
+    gdt_temp.base_middle  = @truncate((base >> 16) & 0xFF);
+    gdt_temp.base_high = @truncate((base >> 24) & 0xFF);
 
-   gdt_entries[num].limit_low = @truncate(limit & 0xFFFF); 
-   gdt_entries[num].granularity = @truncate((limit >> 16) & 0x0F);
+    gdt_temp.limit_low = @truncate(limit & 0xFFFF); 
+    gdt_temp.granularity = @truncate((limit >> 16) & 0x0F);
 
-   gdt_entries[num].granularity |= gran & 0xF0; // take the 4 high bits for limit 
-   gdt_entries[num].access = access;
+    gdt_temp.granularity |= gran & 0xF0; // take the 4 high bits for limit 
+    gdt_temp.access = access;
 }
 
 pub fn gdt_init() void {
@@ -39,7 +41,6 @@ pub fn gdt_init() void {
     gdt_set_entry(2, 0, 0xFFFFFFFF, 0x92, 0xCF); // data segment
     gdt_set_entry(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // stack segment
     gdt_set_entry(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // userspace code
-    // TODO: memcpy the gdt_entries struct into gdt_ptr.base address
     asm volatile (
         \\lgdt (%edi)
         \\jmp $0x08, $.reload_CS
