vasilisalmpanis/kfs

commit 5ba614aee1db340e3b8e2f2ebdcb9ef52d98c700

wip: multiboot
Signed-off-by: Polina Simonenko <rabarbrablad@gmail.com>

src/arch/x86/multiboot.zig
@@ -1,0 +1,22 @@
+pub const multiboot_info = packed struct {
+    flags: u32,
+    mem_lower: u32,
+    mem_upper: u32,
+    boot_device: u32,
+    cmdline: u32,
+    mods_count: u32,
+    mods_addr: u32,
+    syms_0: u32,
+    syms_1: u32,
+    syms_2: u32,
+    syms_3: u32,
+    mmap_length: u32,
+    mmap_addr: u32,
+};
+
+pub const multiboot_memory_map = packed struct {
+    size: u32,
+    // addr: u64,
+    // len: u64,
+    // type: u32,
+};

src/kernel/main.zig
@@ -3,6 +3,7 @@ const TTY = @import("tty.zig");
 const Keyboard = @import("drivers").Keyboard;
 const system = @import("arch").system;
 const gdt = @import("arch").gdt;
+const multiboot = @import("arch").multiboot;
 const screen = @import("screen.zig");
 const debug = @import("debug.zig");
 const printf = @import("printf.zig").printf;
@@ -12,10 +13,21 @@ pub fn trace() void {
     debug.TraceStackTrace(10);
 }
 
-export fn kernel_main() noreturn {
+export fn kernel_main(magic: u32, address: u32) noreturn {
     gdt.gdt_init();
     var scrn : *screen.Screen = screen.Screen.init();
-    // printf("GDT INITIALIZED\n", .{});
+    printf("{x} {x}\n", .{magic, address});
+
+    const info: *multiboot.multiboot_info = @ptrFromInt(address);
+    const mmap: *multiboot.multiboot_memory_map = @ptrFromInt(info.mmap_addr);
+    printf("{}", .{mmap.size});
+    // var i: u32 = 0;
+    // while (i < info.mmap_length): (i += @sizeOf(multiboot.multiboot_memory_map)) {
+    //     const mmap: *multiboot.multiboot_memory_map = @ptrFromInt(info.mmap_addr + i);
+    //     printf("{}", .{mmap});
+    // }
+    printf("{}", .{info});
+    // printf("GDT INITIALIZED {d}\n", .{num});
     inline for (@typeInfo(TTY.ConsoleColors).Enum.fields) |f| {
         const clr: u8 = TTY.vga_entry_color(@field(TTY.ConsoleColors, f.name), TTY.ConsoleColors.Black);
         screen.current_tty.?.print("42\n", clr, false);

src/arch/x86/main.zig
@@ -1,3 +1,4 @@
 pub const io = @import("io.zig");
 pub const system = @import("system/system.zig");
 pub const gdt = @import("gdt.zig");
+pub const multiboot = @import("multiboot.zig");

Makefile
@@ -5,6 +5,7 @@ ISO_DIR = iso
 SRC_DIR = src
 
 SRC = $(shell find $(SRC_DIR) -name '*.zig')
+ASM_SRC = $(shell find $(SRC_DIR) -name '*.s')
 
 all: $(NAME)
 
@@ -12,7 +13,7 @@ $(NAME): $(KERNEL)
 	cp $(KERNEL) $(ISO_DIR)/boot/
 	grub-mkrescue --compress=xz -o $(NAME) $(ISO_DIR)
 
-$(KERNEL): $(SRC)
+$(KERNEL): $(SRC) $(ASM_SRC)
 	zig build # -Doptimize=ReleaseSafe
 
 clean:

linker.ld
@@ -1,4 +1,4 @@
-ENTRY(kernel_main)
+ENTRY(_start)
 
 SECTIONS {
     . = 0x00200000;

src/arch/x86/boot/multiboot.s
@@ -17,13 +17,17 @@ stack_bottom:
 	.skip 16384 # 16 KiB
 stack_top:
 
+.extern kernel_main
 .section .text
 .global _start
 .type _start, @function
 _start:
-	mov $stack_top, %esp
+	// mov $stack_top, %esp
 	xor %ebp, %ebp  /* 0 the ebp register to allow stack traces */
+	push %ebx
+	push %eax
 	call kernel_main
+
 	cli
 1:	hlt
 	jmp 1b

commit 664b1a29ab863ef3374b1fe1ebc98d12f6172d9d

kfs: mm: system memory
use the multiboot information about memory
provided by grub.

Fixes: 5ba614aee1db ("wip: multiboot")

Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

src/arch/x86/multiboot.zig
@@ -14,9 +14,9 @@ pub const multiboot_info = packed struct {
     mmap_addr: u32,
 };
 
-pub const multiboot_memory_map = packed struct {
+pub const multiboot_memory_map = struct {
     size: u32,
-    // addr: u64,
-    // len: u64,
-    // type: u32,
+    addr: [2]u32,
+    len: [2]u32,
+    type: u32,
 };

src/kernel/main.zig
@@ -16,23 +16,23 @@ pub fn trace() void {
 export fn kernel_main(magic: u32, address: u32) noreturn {
     gdt.gdt_init();
     var scrn : *screen.Screen = screen.Screen.init();
-    printf("{x} {x}\n", .{magic, address});
-
-    const info: *multiboot.multiboot_info = @ptrFromInt(address);
-    const mmap: *multiboot.multiboot_memory_map = @ptrFromInt(info.mmap_addr);
-    printf("{}", .{mmap.size});
-    // var i: u32 = 0;
-    // while (i < info.mmap_length): (i += @sizeOf(multiboot.multiboot_memory_map)) {
-    //     const mmap: *multiboot.multiboot_memory_map = @ptrFromInt(info.mmap_addr + i);
-    //     printf("{}", .{mmap});
-    // }
-    printf("{}", .{info});
-    // printf("GDT INITIALIZED {d}\n", .{num});
     inline for (@typeInfo(TTY.ConsoleColors).Enum.fields) |f| {
         const clr: u8 = TTY.vga_entry_color(@field(TTY.ConsoleColors, f.name), TTY.ConsoleColors.Black);
         screen.current_tty.?.print("42\n", clr, false);
     }
-    printf("\n", .{});
+
+    // Verify multiboot magic number
+    if (magic != 0x2BADB002) {
+        printf("Invalid multiboot magic number!\n", .{});
+        while (true) {}
+    }
+
+    const info: *multiboot.multiboot_info = @ptrFromInt(address);
+    var i: u32 = 0;
+    while (i < info.mmap_length) : (i += @sizeOf(multiboot.multiboot_memory_map)) {
+        const mmap: *multiboot.multiboot_memory_map = @ptrFromInt(info.mmap_addr + i);
+        printf("mmap {}\n", .{mmap});
+    }
     var keyboard = Keyboard.init();
 
     while (true) {