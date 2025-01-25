8ffd4d797343c4e5393b19c74c85fe0a19df0869
Polina Simonenko <rabarbrablad@gmail.com>
2025-01-25 17:23:28 +0100

    kfs: mm: vmm: map page done
    Signed-off-by: Polina Simonenko <rabarbrablad@gmail.com>

--- src/arch/x86/mm/vmm.zig
+++ src/arch/x86/mm/vmm.zig
@@ -5,48 +5,60 @@ const PAGE_PRESENT: u8 = 0x1;
 const PAGE_WRITE: u8 = 0x2;
 const PAGE_4MB: u8 = 0x80;
 
-fn print_page_dir() void {
-   var index: u32 = 3;
-   while (index < 768) : (index += 1) {
-       const entry = initial_page_dir[index];
-       if (entry != 0) {
-           const present = (entry & 1) != 0;
-           const writable = (entry & 2) != 0;
-           const user_accessible = (entry & 4) != 0;
-           const write_through = (entry & (1 << 3)) != 0;
-           const cache_disabled = (entry & (1 << 4)) != 0;
-           const accessed = (entry & (1 << 5)) != 0;
-           const dirty = (entry & (1 << 6)) != 0;
-           const is_4mb_page = (entry & (1 << 7)) != 0;
-           const physical_frame = entry & 0xFFFFF000;
+// extern var initial_page_dir: [1024]u32;
+const initial_page_dir: [*]u32 = @ptrFromInt(0xFFFFF000);
 
-           printf("Index {d}: Raw: 0x{x}\n", .{index, entry});
-           printf("  Flags:\n    Present={}, Writable={}, User={}\n    Write-through={}, Cache-disabled={}\n    Accessed={}, Dirty={}, 4MB Page={}\n", 
-               .{present, writable, user_accessible, 
-                 write_through, cache_disabled, 
-                 accessed, dirty, is_4mb_page});
-           printf("  Physical Frame: 0x{x}\n", .{physical_frame});
-       }
-   }
+fn print_page_dir() void {
+    var index: u32 = 0;
+    printf("idx\t\tvirt\traw\t\tphys\t|pres|write|user|wr-th|cach|acc|dirt|4M|\n", .{});
+    while (index < 1024) : (index += 1) {
+        const entry = initial_page_dir[index];
+        if (entry != 0) {
+            const present = (entry & 1);
+            const writable = (entry & 2);
+            const user_accessible = (entry & 4);
+            const write_through = (entry & (1 << 3));
+            const cache_disabled = (entry & (1 << 4));
+            const accessed = (entry & (1 << 5));
+            const dirty = (entry & (1 << 6));
+            const is_4mb_page = (entry & (1 << 7));
+            const physical_frame = entry & 0xFFFFF000;
+
+            printf("{d: >4}\t{x:0>8}\t{x:0>8}\t{x:0>8}\t|{}|{}|{}|{}|{}|{}|{}|{}|\n", .{
+                index,
+                index << 22,
+                entry,
+                physical_frame,
+                present,
+                writable,
+                user_accessible,
+                write_through,
+                cache_disabled,
+                accessed,
+                dirty,
+                is_4mb_page,
+            });
+        }
+    }
 }
 
 pub fn print_page_table(virtual_addr: u32) void {
-    const pd_index = virtual_addr >> 22;          // Page directory index
+    const pd_index = virtual_addr >> 22; // Page directory index
     const page_table_addr = initial_page_dir[pd_index] & 0xFFFFF000;
-    
+
     if (page_table_addr == 0) {
         printf("No page table exists for address 0x{x}\n", .{virtual_addr});
         return;
     }
-    
+
     const page_table: [*]u32 = @ptrFromInt(page_table_addr);
-    
-    printf("Page Table for Address 0x{x} (Page Directory Index: {d}):\n", .{virtual_addr, pd_index});
-    
+
+    printf("Page Table for Address 0x{x} (Page Directory Index: {d}):\n", .{ virtual_addr, pd_index });
+
     for (0..1024) |i| {
         if (page_table[i] != 0) {
             const flags = page_table[i] & 0xFFF;
-            printf("  Entry {d}: Physical Addr: 0x{x}, Flags:\n", .{i, page_table[i] & 0xFFFFF000});
+            printf("  Entry {d}: Physical Addr: 0x{x}, Flags:\n", .{ i, page_table[i] & 0xFFFFF000 });
 
             printf("    Present:       {s}\n", .{if (flags & 1 != 0) "Yes" else "No"});
             printf("    Writable:      {s}\n", .{if (flags & 2 != 0) "Yes" else "No"});
@@ -67,16 +79,15 @@ pub inline fn InvalidatePage(page: usize) void {
     );
 }
 
-extern var initial_page_dir: [1024]u32;
 pub const VMM = struct {
-    pmm : *PMM,
+    pmm: *PMM,
 
     pub fn init(pmm: *PMM) VMM {
-        const vmm = VMM{.pmm = pmm};
+        const vmm = VMM{ .pmm = pmm };
         return vmm;
     }
 
-    // pub fn alloc() u32 {
+    // pub fn alloc(size: u32) u32 {
 
     // }
 
@@ -85,38 +96,36 @@ pub const VMM = struct {
     // }
 
     pub fn map_page(self: *VMM, virtual_addr: u32, physical_addr: u32) void {
-        // 0x00d00000
-        var page_table: [*]u32 = undefined; 
-        const pd_index = virtual_addr >> 22;          // Page directory index
-        const pt_index = (virtual_addr >> 12) & 0x3FF; // Page table index
-        printf("PD_INDEX: {d}\nPT_INDEX: {d}\n",.{pd_index, pt_index});
-        var temp: *u8 = undefined;
-        // _ = physical_addr;
-        // Allocate page table if not exists
-        if (initial_page_dir[pd_index] == 0) {
-            const new_page_table = self.pmm.alloc_page();
-            const new_page_pd_index = (new_page_table >> 20) / 4;
-            initial_page_dir[pd_index] = new_page_table | 3; // Present + writable
-            initial_page_dir[new_page_pd_index] = PAGE_4MB | PAGE_WRITE | PAGE_PRESENT;
-            
-            // printf("content {d}\n", .{temp.*});
-            page_table = @ptrFromInt(new_page_table & 0xFFFFF000);
-            @memset(page_table[0..1024], 0);
+        const pd_idx = virtual_addr >> 22;
+        const pt_idx = (virtual_addr >> 12) & 0x3ff;
+        const pd: [*]u32 = @ptrFromInt(0xFFFFF000);
+        var pt: [*]u32 = undefined;
+
+        if (pd[pd_idx] == 0) {
+            const pt_pfn = self.pmm.alloc_page();
+            const tmp_pd_idx = (pt_pfn >> 20) / 4;
+            pd[pd_idx] = pt_pfn | 3; // Present + writable
+            const tmp = pd[tmp_pd_idx];
+            pd[tmp_pd_idx] = PAGE_4MB | PAGE_WRITE | PAGE_PRESENT;
+            pt = @ptrFromInt(0xFFC00000);
+            pt += (0x400 * pd_idx);
+            @memset(pt[0..1024], 0);
+            pd[tmp_pd_idx] = tmp; // restore initial state of temp page dir
         }
-        // print_page_dir();
-        // // Get page table
-        // We are trying access not mapped memory here. We need to understand how to either map it or how to access with recursice mapping
-        page_table = @ptrFromInt(initial_page_dir[pd_index] & 0xFFFFF000);
-        
-        // // Map page table entry
-        page_table[pt_index] = physical_addr | 3; // Present + writable
+        pt = @ptrFromInt(0xFFC00000);
+        pt += (0x400 * pd_idx);
+        if (pt[pt_idx] != 0)
+            return; // Do something
+        pt[pt_idx] = physical_addr | 0x3;
         InvalidatePage(virtual_addr);
-        print_page_table(virtual_addr);
-        printf("Physical address {x}\n",.{physical_addr});
-        temp = @ptrFromInt(virtual_addr); 
-        temp.* = 42;    
-        printf("temp {d}\n", .{temp.*});
-        print_page_table(virtual_addr);
-        // printf("content in virtual memory {d}\n", .{temp.*});
     }
 };
+
+// Page Directory Entry Structure
+// |00000000|00R00000|000PAAAG|PDAPPURP|
+
+// Virtual addr: 0xdd000000
+// |11011101|00000000|00000000|00000000|
+// PDE Index: 0xdd000000 >> 22 = 884
+// PTE Index: (0xdd000000 >> 12) & 0x3ff = 0
+// Page offset: 0xdd000000 & 0xfff = 0
