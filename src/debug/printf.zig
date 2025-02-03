4736dc276c5cd032d7504c3e97c988f658c43c97
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2025-02-03 19:03:54 +0100

    kfs: framebuffer: fix problems with render
    properly render with framebuffer without crashing and
    use virtual framebuffer to increase performance.
    
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- src/debug/printf.zig
+++ src/debug/printf.zig
@@ -6,10 +6,9 @@ const Writer = @import("std").io.Writer;
 pub const writer = Writer(void, error{}, callback){ .context = {} };
 
 fn callback(_: void, string: []const u8) error{}!usize {
-    const color: u8 = screen.current_tty.?._terminal_color;
     // Print the string passed to the callback
     if (screen.current_tty) |t|
-        t.print(string, color, false);
+        t.print(string, false);
     return string.len;
 }
 
