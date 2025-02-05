9c0df94d8ac14e304df97d7d6e0cead1120eccc5
Polina Simonenko <rabarbrablad@gmail.com>
2025-02-05 18:23:16 +0100

    kfs: mm: heap debug: fixed lost block issue
    Signed-off-by: Polina Simonenko <rabarbrablad@gmail.com>

--- src/debug/tests.zig
+++ src/debug/tests.zig
@@ -3,32 +3,27 @@ const mm = @import("kernel").mm;
 const print_free_list = @import("./mm.zig").print_free_list;
 
 pub fn run_tests() void {
-    var i: u32 = 0;
-    var j: u32 = 0;
-    // fails at size 312
-    // const size: u32 = 312;
-    const size: u32 = 511;
-    var arr: [size]u32 = .{0} ** size;
-    while (i < 2) : (i += 1) {
-        j = 0;
-        while (j < size) : (j += 1) {
-            arr[j] = mm.kmalloc(0);
-            // if (arr[j] - 8 == 0xC1001000)
-                // print_free_list();
-                // printf("{d} addr: {x}\n", .{j, arr[j] - 8});
-        }
-            // printf("addr: {x}\n", .{arr[j]});
-        // print_free_list();
-        j = 0;
-        while (j < size) : (j += 1) {
+    // var i: u32 = 0;
+    // var j: u32 = 0;
 
-            // if (arr[j] - 8 == 0xC1001000)
-                // print_free_list();
-            // printf("addr: {x}\n", .{arr[j]});
-            mm.kfree(arr[j]);
-            // if (arr[j] - 8 == 0xC1001000)
-                // print_free_list();
-        }
-    }
+    printf("Tests are running...\n", .{});
+    var ptr: [*]u8 = @ptrFromInt(mm.kmalloc(128 * 1024));
+    @memcpy(ptr[0..5], "12345");
+    // const size: u32 = 15000;
+    // var arr: [size]u32 = .{0} ** size;
+    // var varr: [size]u32 = .{0} ** size;
+    // while (i < 1) : (i += 1) {
+    //     j = size;
+    //     while (j > 0) : (j -= 1) {
+    //         arr[j - 1] = mm.kmalloc(j);
+    //         varr[j - 1] = mm.vmalloc(j);
+    //     }
+
+    //     j = size;
+    //     while (j > 0) : (j -= 1) {
+    //         mm.kfree(arr[j - 1]);
+    //         mm.vfree(varr[j - 1]);
+    //     }
+    // }
     print_free_list();
 }
--- src/drivers/framebuffer.zig
+++ src/drivers/framebuffer.zig
@@ -14,35 +14,37 @@ pub const FrameBuffer = struct {
         const num_pages = (fb_size + 0xFFF) / mm.PAGE_SIZE;
         var i: u32 = 0;
         var addr = fb_info.?.address & 0xFFFFF000;
-        // var virt_addr: u32 = mm.virt_memory_manager.find_free_space(num_pages, mm.PAGE_OFFSET, 0xFFFFF000, false);
-        // const last_addr: u32 = virt_addr;
-        while (i < num_pages): (i += 1) {
-            // mm.virt_memory_manager.map_page(virt_addr, addr, false);
+        var virt_addr: u32 = mm.virt_memory_manager.find_free_space(
+            num_pages,
+            mm.PAGE_OFFSET,
+            0xFFFFF000,
+            false
+        );
+        const first_addr: u32 = virt_addr;
+        while (i < num_pages) : (i += 1) {
+            mm.virt_memory_manager.map_page(virt_addr, addr, false);
             mm.virt_memory_manager.map_page(addr, addr, false);
-            // virt_addr += mm.PAGE_SIZE;
+            virt_addr += mm.PAGE_SIZE;
             addr += mm.PAGE_SIZE;
         }
         var fb = FrameBuffer{
             .fb_info = fb_info.?,
-            // .fb_ptr = @ptrFromInt(last_addr),
-            .fb_ptr = @ptrFromInt(fb_info.?.address),
+            .fb_ptr = @ptrFromInt(first_addr),
             .cwidth = (fb_info.?.pitch / 4) / 8,
             .cheight = fb_info.?.height / 16,
-            // .virtual_buffer = @ptrFromInt(mm.kmalloc(fb_info.?.width * fb_info.?.height * @sizeOf(u32))),
-            .virtual_buffer = @ptrFromInt(fb_info.?.address),
-        }; 
+            .virtual_buffer = @ptrFromInt(mm.kmalloc(fb_info.?.width * fb_info.?.height * @sizeOf(u32))),
+        };
         fb.clear();
         return fb;
     }
 
     pub fn render(self: *FrameBuffer) void {
-        // const max_index = self.fb_info.height * self.fb_info.width;
-        // @memcpy(self.fb_ptr[0..max_index], self.virtual_buffer[0..max_index]);
-        _ = self;
+        const max_index = self.fb_info.height * self.fb_info.width;
+        @memcpy(self.fb_ptr[0..max_index], self.virtual_buffer[0..max_index]);
     }
 
     pub fn clear(self: *FrameBuffer) void {
-        @memset(self.virtual_buffer[0..self.fb_info.height * self.fb_info.width], 0);
+        @memset(self.virtual_buffer[0 .. self.fb_info.height * self.fb_info.width], 0);
     }
 
     pub fn putchar(
