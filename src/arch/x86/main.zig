e3e0da04f83f361bb0a9e1dc4c69bda2bfc7749c
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2025-02-08 18:06:31 +0100

    kfs: idt: interrupts enabled.
    we can fire interrupt handlers but we need to remap
    interrupts that conflict the exception handlers.
    
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- src/arch/x86/main.zig
+++ src/arch/x86/main.zig
@@ -4,3 +4,4 @@ pub const gdt = @import("gdt.zig");
 pub const multiboot = @import("boot/multiboot.zig");
 pub const vmm = @import("mm/vmm.zig");
 pub const pmm = @import("mm/pmm.zig");
+pub const idt = @import("idt.zig");
