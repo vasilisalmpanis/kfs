c2a966f298f44726a60756de7c811ef0e74994b0
Polina Simonenko <rabarbrablad@gmail.com>
2025-02-10 20:14:56 +0100

    kfs: logger and Serial
    Signed-off-by: Polina Simonenko <rabarbrablad@gmail.com>

--- src/drivers/main.zig
+++ src/drivers/main.zig
@@ -1,4 +1,5 @@
 pub const Keyboard = @import("kbd.zig").Keyboard;
+pub const Serial = @import("serial.zig").Serial;
 pub const screen = @import("./screen.zig");
 pub const tty = @import("./tty.zig");
 pub const shell = @import("./shell.zig");
