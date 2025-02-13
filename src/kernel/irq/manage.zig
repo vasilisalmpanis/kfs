bd6b2643a14860244e998235da6abdccb194af77
Polina Simonenko <rabarbrablad@gmail.com>
2025-02-13 18:55:43 +0100

    kfs: interrupts: separate modulo for kernel, exception handlers
    Signed-off-by: Polina Simonenko <rabarbrablad@gmail.com>

--- src/kernel/irq/manage.zig
+++ src/kernel/irq/manage.zig
@@ -1,7 +1,12 @@
 const arch = @import("arch");
 const MAX_IRQ = 256;
 
-pub const ISRHandler = fn () void;
+/// fn () void;
+pub const ISRHandler = anyopaque;
+
+/// fn (regs: *registers_t) void;
+pub const ExceptionHandler = anyopaque;
+
 
 pub var handlers: [256] ?* const ISRHandler = .{null} ** 256;
 
@@ -15,4 +20,16 @@ pub fn unregister_handler(irq_num: u32) void {
     if (irq_num >= MAX_IRQ - 32)
         @panic("Wrong IRQ number provided");
     handlers[irq_num + 32] = null;
+}
+
+pub fn register_exception_handler(int_num: u32, hndl: *const ExceptionHandler) void {
+    if (int_num > 31)
+        @panic("Wrong exception number");
+    handlers[int_num] = hndl;
+}
+
+pub fn unregister_exception_handler(int_num: u32) void {
+    if (int_num > 31)
+        @panic("Wrong exception number");
+    handlers[int_num] = null;
 }
