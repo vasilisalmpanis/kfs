4d5a29de75fa8b07e90cfe5e2635f9137eee1300
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2025-02-24 19:26:59 +0100

    kfs: utils: intrusive linked list (wip)
    needs testing
    
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

---
+++ src/kernel/utils/list.zig
@@ -0,0 +1,47 @@
+
+const list_head = struct {
+    next: ?*list_head,
+    prev: ?*list_head,
+};
+
+// danger
+pub fn container_of(comptime T: type, ptr: u32, comptime member: []const u8) *T {
+    const offset = @offsetOf(T, member);
+    const result: *T = @ptrFromInt(ptr - offset);
+    return result;
+}
+
+pub fn list_add(new: *list_head, head: *list_head) void {
+    const next = head.next;
+    head.next = new;
+    new.prev = head;
+    new.next = next;
+    next.?.prev = new;
+}
+
+pub fn list_add_tail(new: *list_head, head: *list_head) void {
+    if (head.prev == null) {
+        new.next = head;
+        head.prev = new;
+        new.prev = null;
+        return ;
+    }
+    list_add(new, head.prev.?);
+}
+pub fn list_del(entry: *list_head) void {
+    if (entry.prev == null) {
+        entry.next.?.prev = null;
+        return ;
+    }
+    entry.prev.?.next = entry.next;
+    entry.next.?.prev = entry.prev;
+}
+
+pub fn list_empty(list: *list_head) bool {
+    return list.next == list;
+}
+
+pub fn list_entry(comptime T: type, ptr: u32, comptime member: []const u8) *T {
+    return container_of(T, ptr, member);
+}
+
