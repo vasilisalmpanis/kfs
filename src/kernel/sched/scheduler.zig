1f9ba806264f1b0d3a01851b244f2892329876e6
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2025-03-01 16:00:21 +0100

    kfs: sched: task_struct setup wip
    setup the stack and necessary information of the task
    
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- src/kernel/sched/scheduler.zig
+++ src/kernel/sched/scheduler.zig
@@ -1,28 +1,30 @@
-const task = @import("./task.zig");
+const tsk = @import("./task.zig");
 const dbg = @import("debug");
 const lst = @import("../utils/list.zig");
 
-fn switch_to(task: *task.task_struct) void {
-
+fn switch_to(task: *tsk.task_struct) void {
+    _ = task;
 }
 
 pub fn timer_handler() void {
     dbg.printf("timer\n", .{});
-    if (task.current.children.next) |curr_ptr| {
+    if (tsk.current.children.next) |curr_ptr| {
         const addr: u32 = @intFromPtr(curr_ptr);
         const curr = lst.container_of(
-            task.task_struct,
+            tsk.task_struct,
             addr,
             "siblings"
         );
         if (true or false) {
-            const addr_next: u32 = @intFromPtr(curr.siblings.next);
-            const next = lst.container_of(
-                task.task_struct,
-                addr_next,
-                "siblings"
-            );
-            switch_to(next);
+            if (curr.siblings.next) |next_ptr| {
+                const addr_next: u32 = @intFromPtr(next_ptr);
+                const next = lst.container_of(
+                    tsk.task_struct,
+                    addr_next,
+                    "siblings"
+                );
+                switch_to(next);
+            }
         }
     }
 }
