10938cc7c0d86c78802762d03f23a70e7f078070
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2025-03-07 18:00:28 +0100

    kfs: arch: interrupts
    modify return signature of irq_handler to
    return new state for context switch.
    
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- src/arch/x86/system/cpu.zig
+++ src/arch/x86/system/cpu.zig
@@ -19,21 +19,42 @@ pub const registers_t = struct {
     eflags: u32, 
     useresp: u32, 
     ss: u32,
-};
 
-pub inline fn printRegisters(state: *registers_t) void {
-    krn.logger.INFO("EIP: {X:0>8} ({X:0>8})\n", .{state.eip, 0});
-    
-    krn.logger.INFO("EAX: {X:0>8} EBX: {X:0>8} ECX: {X:0>8} EDX: {X:0>8}\n", 
-        .{state.eax, state.ebx, state.ecx, state.edx});
-    krn.logger.INFO("ESI: {X:0>8} EDI: {X:0>8} EBP: {X:0>8} ESP: {X:0>8}\n",
-        .{state.esi, state.edi, state.ebp, state.esp});
-    krn.logger.INFO("EFLAGS: {X:0>8}\n", .{state.eflags});
-    krn.logger.INFO("gs {X:0>8}\n", .{state.gs});
-    krn.logger.INFO("fs {X:0>8}\n", .{state.fs});
-    krn.logger.INFO("es {X:0>8}\n", .{state.es});
-    krn.logger.INFO("ds {X:0>8}\n", .{state.ds});
-    krn.logger.INFO("cs {X:0>8}\n", .{state.cs});
-    krn.logger.INFO("ss {X:0>8}\n", .{state.ss});
-    
-}
+    pub fn init() registers_t {
+        return registers_t{
+            .gs = 0x10,
+            .fs = 0x10,
+            .es = 0x10,
+            .ds = 0x10,
+            .edi = 0,
+            .esi = 0,
+            .ebp = 0,
+            .esp = 0,
+            .ebx = 0,
+            .edx = 0,
+            .ecx = 0,
+            .eax = 0,
+            .int_no = 0,
+            .err_code = 0,
+            .eip = 0,
+            .cs = 0x8,
+            .eflags = 0,
+            .useresp = 0,
+            .ss = 0,
+        };
+    }
+
+    pub fn dump(self: *registers_t) void {
+        krn.logger.INFO("EIP: {X:0>8} ({X:0>8})\n", .{ self.eip, 0 });
+
+        krn.logger.INFO("EAX: {X:0>8} EBX: {X:0>8} ECX: {X:0>8} EDX: {X:0>8}\n", .{ self.eax, self.ebx, self.ecx, self.edx });
+        krn.logger.INFO("ESI: {X:0>8} EDI: {X:0>8} EBP: {X:0>8} ESP: {X:0>8}\n", .{ self.esi, self.edi, self.ebp, self.esp });
+        krn.logger.INFO("EFLAGS: {X:0>8}\n", .{self.eflags});
+        krn.logger.INFO("gs {X:0>8}\n", .{self.gs});
+        krn.logger.INFO("fs {X:0>8}\n", .{self.fs});
+        krn.logger.INFO("es {X:0>8}\n", .{self.es});
+        krn.logger.INFO("ds {X:0>8}\n", .{self.ds});
+        krn.logger.INFO("cs {X:0>8}\n", .{self.cs});
+        krn.logger.INFO("ss {X:0>8}\n", .{self.ss});
+    }
+};
