2d1e61dabed865a534e74eae80595f623edcfbde
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2025-03-18 15:16:50 +0100

    kfs: idt refactor: fix typos
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- src/kernel/irq/manage.zig
+++ src/kernel/irq/manage.zig
@@ -10,25 +10,25 @@ pub const ExceptionHandler = anyopaque;
 pub var handlers: [arch.IDT_MAX_DESCRIPTORS] ?* const ISRHandler = .{null} ** arch.IDT_MAX_DESCRIPTORS;
 
 pub fn register_handler(irq_num: u32, hndl: *const ISRHandler) void {
-    if (irq_num >= arch.IDT_MAX_DESCRIPTORS - arch.CPU_EXPECTIONS_COUNT)
+    if (irq_num >= arch.IDT_MAX_DESCRIPTORS - arch.CPU_EXCEPTION_COUNT)
         @panic("Wrong IRQ number provided");
-    handlers[irq_num + arch.CPU_EXPECTIONS_COUNT] = hndl;
+    handlers[irq_num + arch.CPU_EXCEPTION_COUNT] = hndl;
 }
 
 pub fn unregister_handler(irq_num: u32) void {
-    if (irq_num >= arch.IDT_MAX_DESCRIPTORS - arch.CPU_EXPECTIONS_COUNT)
+    if (irq_num >= arch.IDT_MAX_DESCRIPTORS - arch.CPU_EXCEPTION_COUNT)
         @panic("Wrong IRQ number provided");
-    handlers[irq_num + arch.CPU_EXPECTIONS_COUNT] = null;
+    handlers[irq_num + arch.CPU_EXCEPTION_COUNT] = null;
 }
 
 pub fn register_exception_handler(int_num: u32, hndl: *const ExceptionHandler) void {
-    if (int_num >= arch.CPU_EXPECTIONS_COUNT)
+    if (int_num >= arch.CPU_EXCEPTION_COUNT)
         @panic("Wrong exception number");
     handlers[int_num] = hndl;
 }
 
 pub fn unregister_exception_handler(int_num: u32) void {
-    if (int_num >= arch.CPU_EXPECTIONS_COUNT)
+    if (int_num >= arch.CPU_EXCEPTION_COUNT)
         @panic("Wrong exception number");
     handlers[int_num] = null;
 }
