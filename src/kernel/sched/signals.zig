8c411734f5003182efc53e2bde150aaa5415d422
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2025-03-29 19:29:38 +0100

    kfs: userspace dirty wip
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- src/kernel/sched/signals.zig
+++ src/kernel/sched/signals.zig
@@ -1,3 +1,7 @@
+const tsk = @import("./task.zig");
+const krn = @import("../main.zig");
+const std = @import("std");
+const arch = @import("arch");
 pub const SIG_COUNT: u8 = 32;
 
 const SigHandler = fn (signum: u8) void;
@@ -40,8 +44,94 @@ const Signal = enum(u8) {
     SIGUNUSED,      // Dump        Equivalent to SIGSYS                 No
 };
 
+const SignalTerminated = std.EnumMap(Signal, bool).init(.{
+    .EMPTY      = false,
+    .SIGHUP     = true,
+    .SIGINT     = true,
+    .SIGQUIT    = false,
+    .SIGILL     = false,
+    .SIGTRAP    = false,
+    .SIGABRT    = false,
+    .SIGIOT     = false,
+    .SIGBUS     = false,
+    .SIGFPE     = false,
+    .SIGKILL    = true,
+    .SIGUSR1    = true,
+    .SIGSEGV    = false,
+    .SIGUSR2    = true,
+    .SIGPIPE    = true,
+    .SIGALRM     = true,
+    .SIGTERM     = true,
+    .SIGSTKFLT  = true,
+    .SIGCHLD    = false,
+    .SIGCONT    = false,
+    .SIGSTOP    = false,
+    .SIGTSTP    = false,
+    .SIGTTIN    = false,
+    .SIGTTOU    = false,
+    .SIGURG     = false,
+    .SIGXCPU    = false,
+    .SIGXFSZ    = false,
+    .SIGVTALRM  = true,
+    .SIGPROF    = true,
+    .SIGWINCH   = false,
+    .SIGIO      = true,
+    .SIGPOLL    = true,
+    .SIGPWR     = true,
+    .SIGSYS     = false,
+    .SIGUNUSED  = false,
+});
 fn sigHandler(signum: u8) void {
     _ = signum;
+    krn.logger.WARN("Hup\n", .{});
+}
+
+pub fn signalWrapper() void {
+    asm volatile ("pop %ebp;");
+    const stack: u32 = arch.cpu.getESP();
+    const eip: u32 = tsk.current.sig_eip;
+    // const pending = tsk.current.sig_pending;
+    tsk.current.sig_pending = 0;
+    asm volatile (arch.idt.push_regs);
+    asm volatile(
+        \\ xor %eax, %eax
+        \\ xor %ebx, %ebx
+        \\ xor %ecx, %ecx
+    );
+    // if (tsk.current.sigaction.deliverSignals(pending)) {
+        // @import("./scheduler.zig").reschedule();
+    // }
+    // arch.io.outb(0x3F8, 'a');
+    asm volatile (arch.idt.pop_regs);
+    tsk.current.sig_eip = 0;
+    asm volatile (
+        \\ cli
+        \\ push $0x10
+        \\ push %[stack]
+        \\ pushf
+        \\ pop %ebx
+        \\ or $0x200, %ebx
+        \\ push %ebx
+        \\ push $0x8
+        \\ push %[eip]
+        \\ iret
+        :: [stack] "r" (stack), [eip] "r" (eip)
+        :
+    );
+}
+
+fn sigHup(_: u8) void {
+    // Terminating.
+    // tsk.current.state = .STOPPED;
+    // tsk.current.list.del();
+    // if (tsk.stopped_tasks == null) {
+    //     tsk.stopped_tasks = &tsk.current.list;
+    //     tsk.stopped_tasks.?.setup();
+    // } else {
+    //     tsk.stopped_tasks.?.addTail(&tsk.current.list);
+    // }
+    // tsk.tasks_mutex.unlock();
+    krn.logger.WARN("Hup\n", .{});
 }
 
 pub const SigAction = struct {
@@ -52,7 +142,21 @@ pub const SigAction = struct {
         for (0..SIG_COUNT) |idx| {
             sigaction.sig_handlers[idx] = &sigHandler;
         }
+        sigaction.sig_handlers[1] = &sigHup;
         return sigaction;
+    }
+
+    pub fn deliverSignals(self: *SigAction, pending: u32) bool {
+        var temp: u32 = pending;
+        for(0..32) |i| {
+            temp = pending >> @as(u5, @truncate(i));
+            if (temp & 0x1 > 0) {
+                self.sig_handlers[i](@intCast(i));
+                if (SignalTerminated.get(@enumFromInt(i)) orelse false)
+                    return true;
+            }
+        }
+        return false;
     }
 };
 
--- src/userspace/user.asm
+++ src/userspace/user.asm
@@ -3,6 +3,7 @@ BITS 32
 userspace:
     mov eax, 62
     mov ebx, 3
-    mov ecx, 31
+    mov ecx, 1
     int 0x80
-    jmp userspace
+holder:
+    jmp holder
