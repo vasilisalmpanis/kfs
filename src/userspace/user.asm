ea9f6b2a3e815fee7d70f65201b6d03df0fd14c6
Polina Simonenko <rabarbrablad@gmail.com>
2025-03-30 12:32:51 +0200

    kfs: signals: reorganized SigAction struct
    (wip - signals are broken)
    
    Signed-off-by: Polina Simonenko <rabarbrablad@gmail.com>

--- src/userspace/user.asm
+++ src/userspace/user.asm
@@ -2,8 +2,12 @@ BITS 32
 
 userspace:
     mov eax, 62
-    mov ebx, 3
+    mov ebx, 2
     mov ecx, 1
     int 0x80
+    mov eax, 62
+    mov ebx, 2
+    mov ecx, 31
+    int 0x80
 holder:
-    jmp holder
+    jmp userspace
