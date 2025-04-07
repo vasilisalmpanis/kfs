cf9f233c4709f16aebc0e208b3fef150d644ed64
Polina Simonenko <rabarbrablad@gmail.com>
2025-04-07 13:32:28 +0200

    kfs: vmm: changes in previous commit are breaking heap.alloc().
    So, debug is needed.
    
    Fixed debug.printMapped()
    Moved initKernelSpace to vmm.init() (where it should be)
    
    Signed-off-by: Polina Simonenko <rabarbrablad@gmail.com>

--- src/kernel/mm/kmalloc.zig
+++ src/kernel/mm/kmalloc.zig
@@ -1,11 +1,15 @@
 const mm = @import("init.zig");
+const krn = @import("../main.zig");
 
 // TODO: Fix Alignment for memery returned to 16
 /// kmalloc - allocate memory
 /// size:
 ///     how many bytes of memory are required.
 pub fn kmalloc(size: u32) u32 {
-    return mm.kheap.alloc(size, true, false) catch 0;
+    krn.logger.INFO("kmalloc: {d}", .{size});
+    const res = mm.kheap.alloc(size, true, false) catch 0;
+    krn.logger.INFO("kmalloc done: {d}", .{size});
+    return res;
 }
 
 pub fn kfree(addr: u32) void {
