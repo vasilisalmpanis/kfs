e1453b6e7977adca23a13998fe18ef12436668f4
Polina Simonenko <rabarbrablad@gmail.com>
2025-04-07 15:08:50 +0200

    kfs: vmm: initialize page tables to 0
    Signed-off-by: Polina Simonenko <rabarbrablad@gmail.com>

--- src/kernel/mm/kmalloc.zig
+++ src/kernel/mm/kmalloc.zig
@@ -1,15 +1,11 @@
 const mm = @import("init.zig");
-const krn = @import("../main.zig");
 
 // TODO: Fix Alignment for memery returned to 16
 /// kmalloc - allocate memory
 /// size:
 ///     how many bytes of memory are required.
 pub fn kmalloc(size: u32) u32 {
-    krn.logger.INFO("kmalloc: {d}", .{size});
-    const res = mm.kheap.alloc(size, true, false) catch 0;
-    krn.logger.INFO("kmalloc done: {d}", .{size});
-    return res;
+    return mm.kheap.alloc(size, true, false) catch 0;
 }
 
 pub fn kfree(addr: u32) void {
