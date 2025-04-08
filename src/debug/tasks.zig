a02204e6064c831aee7bbf4ec0f9078097db5331
Polina Simonenko <rabarbrablad@gmail.com>
2025-04-08 13:30:20 +0200

    kfs: better ps
    Signed-off-by: Polina Simonenko <rabarbrablad@gmail.com>

--- src/debug/tasks.zig
+++ src/debug/tasks.zig
@@ -5,29 +5,42 @@ const writer = @import("./printf.zig").writer;
 const fmt = @import("std").fmt;
 const krn = @import("kernel");
 const arch = @import("arch");
+const lookupSymbol = @import("./symbols.zig").lookupSymbol;
+const std = @import("std");
+
+fn printTask(task: *tsk.Task) void {
+    var buffer: [10:0]u8 = .{'0', 'x' } ++ .{0} ** 8;
+        const name: []const u8 = if (task.threadfn) |f| if_lbl: {
+        break :if_lbl if (lookupSymbol(@intFromPtr(f))) |s| s else "";
+    } else else_lbl: {
+        _ = std.fmt.formatIntBuf(
+            buffer[2..],
+            task.regs.eip,
+            16,
+            .upper,
+            .{.alignment = .right, .width = 8, .fill = '0' }
+        );
+        break :else_lbl &buffer;
+    };
+    printf("{d}: {s} {s} {s} {s}\n", .{
+        task.pid,
+        @tagName(task.state),
+        @tagName(task.tsktype),
+        if (task.regs.cs == arch.idt.KERNEL_CODE_SEGMENT) "KRN" else "USR",
+        name,
+    });
+}
 
 pub fn ps() void {
     var it = tsk.initial_task.list.iterator();
     while (it.next()) |i| {
-        const task = i.curr.entry(tsk.Task, "list");
-        printf("{d}: {s} {s} {s}\n", .{
-            task.pid,
-            @tagName(task.state),
-            @tagName(task.tsktype),
-            if (task.regs.cs == arch.idt.KERNEL_CODE_SEGMENT) "kernel" else "user",
-        });
+        printTask(i.curr.entry(tsk.Task, "list"));
     }
     if (tsk.stopped_tasks) |stopped| {
         printf("===STOPPED===\n", .{});
         it = stopped.iterator();
         while (it.next()) |i| {
-            const task = i.curr.entry(tsk.Task, "list");
-            printf("{d}: {s} {s} {s}\n", .{
-                task.pid,
-                @tagName(task.state),
-                @tagName(task.tsktype),
-                if (task.regs.cs == arch.idt.KERNEL_CODE_SEGMENT) "kernel" else "user",
-            });
+            printTask(i.curr.entry(tsk.Task, "list"));
         }
     }
 }
