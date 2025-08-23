81e28d84d205249ce899fa4ab6fb999b50d54344
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2025-08-23 15:17:50 +0200

    kfs: ata: add ata devices
    scan for ata disks if the ide controller exists and
    add them to the bus.
    
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- src/drivers/ata/driver.zig
+++ src/drivers/ata/driver.zig
@@ -1,38 +1,40 @@
-const drv = @import("../main.zig");
-const pci = drv.pci;
+const Bus = @import("../bus.zig").Bus;
+const drv = @import("../driver.zig");
+const dev = @import("../device.zig");
+const utils = @import("kernel").list;
+const std = @import("std");
 const kernel = @import("kernel");
+const ATADevice = @import("./device.zig").ATADevice;
+const bus = @import("./bus.zig");
 
-var ata_driver = pci.PCIDriver{
-    .driver = drv.driver.Driver{
-        .fops = null,
-        .list = undefined,
-        .name = "ata",
-        .probe = undefined,
-        .remove = undefined,
-    },
-    .ids = ids,
-    .probe = ata_probe,
-    .remove = ata_remove,
+pub const ATADriver = struct {
+    driver: drv.Driver,
+
+    probe: *const fn(*ATADevice) anyerror!void,
+    remove: *const fn(*ATADevice) anyerror!void,
 };
 
-const ids: [] const pci.driver.PCIid = &[_]pci.driver.PCIid{
-    pci.driver.PCIid{
-       .class = 0x1, 
-       .subclass = 0x1,
-       .deviceid = 0x7010,
-       .vendorid = 0x8086,
-    },
-};
+// core probe wrapper
+fn ata_probe_device(driver: *drv.Driver, device: *dev.Device) anyerror!void {
+    const ata_dev: *ATADevice = @fieldParentPtr("dev", device);
+    const ata_driver: *ATADriver = @fieldParentPtr("driver", driver);
 
-fn ata_probe(device: *pci.PCIDevice) !void {
-    kernel.logger.WARN("ATA DEVICE {s}\n", .{device.dev.name});
+    try ata_driver.probe(ata_dev);
 }
 
-fn ata_remove(_: *pci.PCIDevice) !void {
+fn ata_remove_device(driver: *drv.Driver, device: *dev.Device) anyerror!void {
+    const ata_dev: *ATADevice = @fieldParentPtr("dev", device);
+    const ata_driver: *ATADriver = @fieldParentPtr("driver", driver);
+
+    try ata_driver.remove(ata_dev);
 }
 
-pub fn init() void {
-    pci.driver.pci_register_driver(&ata_driver.driver) catch {
-        kernel.logger.ERROR("ATA driver cannot be registered\n", .{});
-    };
+pub fn ata_register_driver(driver: *drv.Driver) !void {
+    driver.probe = ata_probe_device;
+    driver.remove = ata_remove_device;
+    try driver.register(&bus.ata_bus);
+}
+
+pub fn ata_unregister_driver(driver: *drv.Driver) void {
+    driver.unregister(&bus.ata_bus);
 }
