5013864f35ec3f1c5346cbd38c1ea455e1bf9166
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2025-08-29 19:46:49 +0200

    kfs: read: fix logger crash
    current.files.get returns some trash
    that logger dereferences. Remove the logger.
    
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- src/kernel/syscalls/read.zig
+++ src/kernel/syscalls/read.zig
@@ -5,7 +5,6 @@ const krn = @import("../main.zig");
 
 pub fn read(fd: u32, buf: u32, size: u32) !u32 {
     const data: [*]u8 = @ptrFromInt(buf);
-    krn.logger.ERROR("READING FROM {d} {any}\n", .{fd, krn.task.current.files.fds.get(fd)});
     if (krn.task.current.files.fds.get(fd)) |file| {
         return try file.ops.read(file, data, size);
     }
