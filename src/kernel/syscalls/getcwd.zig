457a6ad515bb3f4f5dfd288b5829a60694a8fa9f
Polina Simonenko <rabarbrablad@gmail.com>
2025-09-12 09:41:25 +0200

    kfs: fs: getcwd syscall
    Signed-off-by: Polina Simonenko <rabarbrablad@gmail.com>

---
+++ src/kernel/syscalls/getcwd.zig
@@ -0,0 +1,16 @@
+const krn = @import("../main.zig");
+const errors = @import("error-codes.zig").PosixError;
+
+pub fn getcwd(buf: ?[*:0]u8, size: usize) !u32 {
+    if (buf == null or size < 2) {
+        return errors.ERANGE;
+    }
+    const user_buf = buf.?;
+    const buf_s = user_buf[0..size];
+    buf_s[0] = '/';
+    buf_s[1] = 0;
+    const res = krn.task.current.fs.pwd.getAbsPath(buf_s) catch {
+        return errors.EFAULT;
+    };
+    return @intFromPtr(res.ptr);
+}
