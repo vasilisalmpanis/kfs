a3078cd445a8d9a83616df1a2e74abf4efe500df
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2025-09-13 20:06:02 +0200

    kfs: process:
    - clone open file descriptors during forking.
    - release current pwd and root during exit and free the struct
    - unref open files during exit and free the struct
    - copy fds 0,1,2 during TaskFiles.dup
    - make use fork in userspace. (dangerous because we could exit from pid 1)
    
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- src/kernel/syscalls/close.zig
+++ src/kernel/syscalls/close.zig
@@ -2,11 +2,14 @@ const tsk = @import("../sched/task.zig");
 const errors = @import("./error-codes.zig").PosixError;
 const arch = @import("arch");
 const sockets = @import("../net/socket.zig");
+const kernel = @import("../main.zig");
 
 pub fn close(fd: u32) !u32 {
     if (tsk.current.files.releaseFD(fd)) {
+        kernel.logger.INFO("finished closing {d}\n", .{fd});
         return 0;
     }
+    kernel.logger.INFO("error closing {d}\n", .{fd});
     return errors.EBADF;
     // if (sockets.findById(fd)) |socket| {
     //     socket.delete();
--- src/kernel/syscalls/exit.zig
+++ src/kernel/syscalls/exit.zig
@@ -2,8 +2,29 @@ const tsk = @import("../sched/task.zig");
 const arch = @import("arch");
 const sched = @import("../sched/scheduler.zig");
 const signals = @import("../sched/signals.zig");
+const kernel = @import("../main.zig");
+const errors = @import("./error-codes.zig").PosixError;
 
 pub fn exit(error_code: i32) !u32 {
+    if (tsk.current == &tsk.initial_task) return errors.EINVAL;
+    const files: *kernel.fs.TaskFiles = tsk.current.files;
+    var it = files.map.iterator(.{.direction = .forward, .kind = .set});
+    while (it.next()) |idx| {
+        if (files.fds.fetchRemove(idx)) |kv| {
+            const file: *kernel.fs.File = kv.value;
+            // We only call close when no one is using the file
+            file.ref.unref(); // This should close and free the file if 0
+        }
+    }
+    tsk.current.files.fds.deinit();
+    tsk.current.files.map.deinit();
+    tsk.current.fs.pwd.release();
+    tsk.current.fs.root.release();
+
+    // TODO: Big one. Free memory mappings.
+    // This means that in mm dup we already copied the mappings
+    // which we don't do. We need to start by fixing this for fork
+    // and then free them here.
     tsk.current.result = error_code;
     tsk.current.state = .ZOMBIE;
     if (tsk.current.tree.parent) |p| {
