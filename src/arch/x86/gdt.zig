3403bc14d6879f124133fe8bcc54f061e5b8eddb
Polina Simonenko <rabarbrablad@gmail.com>
2025-09-15 23:56:59 +0200

    kfs: libc: libc initialization works now (wip)
    Signed-off-by: Polina Simonenko <rabarbrablad@gmail.com>

--- src/arch/x86/gdt.zig
+++ src/arch/x86/gdt.zig
@@ -18,6 +18,10 @@ pub const GdtEntry = packed struct {
 
 pub var gdt_ptr : Gdtr = undefined;
 
+pub const GDT_TLS0_INDEX: u32 = 6;
+pub const GDT_TLS1_INDEX: u32 = 7;
+pub const GDT_TLS2_INDEX: u32 = 8;
+
 pub fn gdtSetEntry(num: u32, base: u32, limit: u32, access: u8, gran: u8) void {
     // load the entry using num as an index into GDTBASE (0x00000800)
     var gdt_temp: *GdtEntry = @ptrFromInt(GDTBASE + (num * @sizeOf(GdtEntry)));
@@ -39,7 +43,7 @@ pub fn gdtInit() void {
     tss.ss0 = idt.KERNEL_DATA_SEGMENT;
     tss.esp0 = @intFromPtr(&stack_top);
 
-    gdt_ptr.limit = (@sizeOf(GdtEntry) * 6) - 1;
+    gdt_ptr.limit = (@sizeOf(GdtEntry) * 9) - 1;
     gdt_ptr.base = GDTBASE;
 
     gdtSetEntry(0,0,0,0,0);                    // Null segment
@@ -48,6 +52,10 @@ pub fn gdtInit() void {
     gdtSetEntry(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // userspace code
     gdtSetEntry(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // userspace data
     gdtSetEntry(5, @intFromPtr(&tss), @sizeOf(cpu.TSS) - 1, 0x89, 0x00); // userspace stack
+
+    gdtSetEntry(GDT_TLS0_INDEX, 0, 0, 0x72, 0x00);
+    gdtSetEntry(GDT_TLS1_INDEX, 0, 0, 0x72, 0x00);
+    gdtSetEntry(GDT_TLS2_INDEX, 0, 0, 0x72, 0x00);
 
     asm volatile (
         \\  lgdt (%edi)
