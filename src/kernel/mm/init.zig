3b6bc835ab9b350fdc5a7c3025afff689cff6e3b
Vasileios Almpanis <valmpanis@vasileios.almpanis>
2025-09-17 15:30:11 +0200

    kfs: mm: create memory mappings for userspace sections
    instead of hardcoding the allocation of sections
    and not tracking the memory anywhere use mmap_area
    that automatically maps memory for use by userspace.
    This is preliminary commit before we convert
    virtual address space deletion to vma driven.
    Currently we just iterate over all the PDE and PTE
    to find what we need to remove.
    
    Signed-off-by: Vasileios Almpanis <valmpanis@vasileios.almpanis>

--- src/kernel/mm/init.zig
+++ src/kernel/mm/init.zig
@@ -11,6 +11,10 @@ const dbg = @import("debug");
 
 pub const proc_mm = @import("./proc_mm.zig");
 pub const MM = @import("./proc_mm.zig").MM;
+pub const MAP = @import("./proc_mm.zig").MAP;
+pub const PROC_READ = @import("./proc_mm.zig").PROT_READ;
+pub const PROC_WRITE = @import("./proc_mm.zig").PROT_WRITE;
+pub const PROC_RW: u32 = PROC_READ | PROC_WRITE;
 
 pub const kmalloc = @import("./kmalloc.zig").kmalloc;
 pub const kmallocArray = @import("./kmalloc.zig").kmallocArray;
