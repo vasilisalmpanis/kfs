3e85b615aea5dc686b91b2f67812ea61674be70a
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2026-01-16 12:50:59 +0100

    kfs: modules: /proc/modules
    Fix the output of /proc/modules so
    userspace tools such as lsmod works with it.
    
    Adds symlinks to insmod, lsmod ,rmmod for testing
    
    - lsmod works
    - insmod works with absolute path
    - rmmod doesn't work (TODO)
    
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- src/kernel/fs/proc/modules_info.zig
+++ src/kernel/fs/proc/modules_info.zig
@@ -12,40 +12,48 @@ const Slice = struct {
 
 fn modules_open(base: *kernel.fs.File, inode: *kernel.fs.Inode) anyerror!void {
     _ = inode;
+
     modules.loader.modules_mutex.lock();
     defer modules.loader.modules_mutex.unlock();
-    if (modules.loader.modules_list == null)
-        return ;
-    var size: u32 = 0;
-    if (modules.loader.modules_list) |head| {
+
+    const head_opt = modules.loader.modules_list;
+    if (head_opt == null)
+        return;
+
+    const head = head_opt.?;
+
+    var size: usize = 0;
+    {
         var it = head.list.iterator();
         while (it.next()) |node| {
             const module = node.curr.entry(modules.loader.Module, "list");
-            size += module.name.len;
-            size += 1; // space
-            size += std.fmt.count("{d}", .{module.code.len});
-            size += 1; // newline
+            size += std.fmt.count(
+                "{s} {d} 0 - Live 0x00000000\n",
+                .{ module.name, module.code.len },
+            );
         }
-        it = head.list.iterator();
-        const buffer = kernel.mm.kmallocSlice(u8, size) orelse {
-            return kernel.errors.PosixError.ENOMEM;
-        };
-        errdefer kernel.mm.kfree(buffer.ptr);
-        var offset: u32 = 0;
+    }
+
+    const buffer = kernel.mm.kmallocSlice(u8, size) orelse
+        return kernel.errors.PosixError.ENOMEM;
+    errdefer kernel.mm.kfree(buffer.ptr);
+
+    var offset: usize = 0;
+    {
+        var it = head.list.iterator();
         while (it.next()) |node| {
             const module = node.curr.entry(modules.loader.Module, "list");
-            @memcpy(buffer[offset..offset + module.name.len], module.name[0..]);
-            offset += module.name.len;
-            buffer[offset] = ' ';
-            offset += 1;
-            const current_size =  std.fmt.count("{d}", .{module.code.len});
-            _ = try std.fmt.bufPrint(buffer[offset..offset + current_size], "{d}", .{module.code.len});
-            offset += current_size;
-            buffer[offset] = '\n';
-            offset += 1;
+
+            const written = try std.fmt.bufPrint(
+                buffer[offset..],
+                "{s} {d} 0 - Live 0x00000000\n",
+                .{ module.name, module.code.len },
+            );
+            offset += written.len;
         }
-        try generic_ops.assignSlice(base, buffer);
     }
+
+    try generic_ops.assignSlice(base, buffer);
 }
 
 const modules_ops = kernel.fs.FileOps{
