3189bfc9b5a028e37a2a1ce1b45d9c1a5fdc0b37
Polina Simonenko <rabarbrablad@gmail.com>
2026-02-01 15:21:38 +0100

    kfs: getdents: fixed alignment and offset bugs
    Signed-off-by: Polina Simonenko <rabarbrablad@gmail.com>

--- src/kernel/syscalls/getdents.zig
+++ src/kernel/syscalls/getdents.zig
@@ -16,25 +16,41 @@ pub fn getdents64(fd: u32, dirents: [*]u8, size: u32) !u32 {
                     const ret =  try readdir(dir_file, buf_slice);
                     var offset: u32 = 0;
                     var u_off: u32 = 0;
-                    var dirent: *krn.fs.LinuxDirent = @ptrCast(@alignCast(buf_slice.ptr));
-                    const ent_size = @sizeOf(krn.fs.Dirent64) - 1;
+                    
+                    // @sizeOf() returns 20 but name starts at offset 19
+                    const header_size: u32 = @sizeOf(krn.fs.Dirent64) - 1;                    
                     while (offset < ret) {
+                        const dirent: *krn.fs.LinuxDirent = @ptrCast(
+                            @alignCast(&buf_slice[offset])
+                        );
+                        const entry_name: []u8 = dirent.getName();                        
+                        var entry_size: u32 = header_size + @as(
+                            u32, @intCast(entry_name.len)
+                        ) + 1;
+                        entry_size = (entry_size + 7) & ~@as(u32, 7);                        
+                        if (u_off + entry_size > size) {
+                            break;
+                        }
                         var temp = krn.fs.Dirent64{
                             .ino = @intCast(dirent.ino),
                             .off = @intCast(dirent.off),
-                            .reclen = dirent.reclen + (@sizeOf(krn.fs.Dirent64) - @sizeOf(krn.fs.LinuxDirent)), 
+                            .reclen = @intCast(entry_size),
                             .type = dirent.type,
                         };
-                        const name_start: u32 = u_off + ent_size;
-                        @memcpy(dirents[u_off..name_start], @as([*]u8, @ptrCast(&temp))[0..ent_size]);
-                        const entry_name: []u8 = dirent.getName();
-                        @memcpy(dirents[name_start..name_start + entry_name.len], entry_name);
-                        dirents[name_start + entry_name.len] = 0;
-                        u_off += temp.reclen;
-                        if (u_off > size)
-                            break;
-                        offset = offset + dirent.reclen;
-                        dirent = @ptrCast(@alignCast(&buf_slice[offset]));
+                        
+                        @memcpy(
+                            dirents[u_off..u_off + header_size],
+                            @as([*]u8, @ptrCast(&temp))[0..header_size]
+                        );                        
+                        const name_start: u32 = u_off + header_size;
+                        @memcpy(
+                            dirents[name_start..name_start + @as(u32, @intCast(entry_name.len))],
+                            entry_name
+                        );
+                        dirents[name_start + @as(u32, @intCast(entry_name.len))] = 0;
+                        
+                        u_off += entry_size;
+                        offset += dirent.reclen;
                     }
                     return u_off;
                 }
