a084c7a2e22c93cd1260649bdc7c2ee288dd6e94
Polina Simonenko <rabarbrablad@gmail.com>
2026-02-01 15:30:25 +0100

    kfs: syscalls: brk: fixed brk behavior to return correct value
    Signed-off-by: Polina Simonenko <rabarbrablad@gmail.com>

--- src/kernel/syscalls/brk.zig
+++ src/kernel/syscalls/brk.zig
@@ -4,36 +4,64 @@ const krn = @import("../main.zig");
 const arch = @import("arch");
 
 pub fn brk(addr: u32) !u32 {
-    const current_brk = krn.task.current.mm.?.brk;
+    const mm = krn.task.current.mm
+        orelse return 0;
+    const current_brk = mm.brk;
 
-    var req = addr;
     // If addr is 0, return current break (heap end)
-    if (req == 0) {
+    if (addr == 0) {
         krn.logger.INFO("brk(0) returning current heap: 0x{x}\n", .{current_brk});
         return current_brk;
     }
-    if (req < krn.task.current.mm.?.brk_start) {
-        req = krn.task.current.mm.?.brk_start;
+
+    var req = addr;
+    if (req < mm.brk_start) {
+        req = mm.brk_start;
     }
+
+    // Shrinking the brk
     if (req < current_brk) {
-        _ = try krn.do_munmap(krn.task.current.mm.?, req, current_brk);
-        krn.task.current.mm.?.brk = req;
-        return 0;
+        const aligned_req = arch.pageAlign(req, false);
+        const aligned_current = arch.pageAlign(current_brk, false);
+        if (aligned_req < aligned_current) {
+            _ = krn.do_munmap(mm, aligned_req, aligned_current) catch {
+                return current_brk;
+            };
+        }
+        mm.brk = req;
+        krn.logger.INFO("brk shrinking to 0x{x}\n", .{req});
+        return req;
     }
+
     if (req == current_brk)
-        return 0;
-    const len = arch.pageAlign(req - current_brk, false);
-    const new_brk = try krn.task.current.mm.?.mmap_area(current_brk,
-        len,
-        krn.mm.PROT_RW, 
-        krn.mm.MAP{
-            .TYPE = .PRIVATE,
-            .ANONYMOUS = true,
-        },
-        null,
-        0
-    );
-    // TODO: fix brk cuz its broken
-    krn.task.current.mm.?.brk = new_brk;
-    return 0;
+        return current_brk;
+
+    // Expanding the brk
+    const aligned_current = arch.pageAlign(current_brk, false);
+    const aligned_req = arch.pageAlign(req, false);
+    
+    if (aligned_req > aligned_current) {
+        const len = aligned_req - aligned_current;
+        _ = mm.mmap_area(
+            aligned_current,
+            len,
+            krn.mm.PROT_RW,
+            krn.mm.MAP{
+                .TYPE = .PRIVATE,
+                .ANONYMOUS = true,
+                .FIXED = true,
+            },
+            null,
+            0
+        ) catch {
+            krn.logger.WARN(
+                "brk expansion failed, returning current: 0x{x}\n",
+                .{current_brk}
+            );
+            return current_brk;
+        };
+    }
+    
+    mm.brk = req;
+    return req;
 }
