f07c0ca78bf6753fc4f23845b53b7e3bc0e93a92
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2026-03-19 19:58:50 +0100

    kfs: syscalls: fix optional argument deref in read/write
    NULL could be passed as 2nd argument to read/write + variations
    which leads to kernel panic due to null ptr deref. Add the appropriate
    checks. According to the manual for both read and write first some basic
    errors are checked and then if size is 0 zero is returned. Testing
    manually revealed that if you pass NULL and size 0, still 0 is returned
    so that behavior is mimicked.
    
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- src/kernel/syscalls/write.zig
+++ src/kernel/syscalls/write.zig
@@ -1,39 +1,42 @@
-const errors = @import("./error-codes.zig");
+const errors = @import("./error-codes.zig").PosixError;
 const arch = @import("arch");
 const dbg = @import("debug");
 const krn = @import("../main.zig");
 
-pub fn write(fd: u32, buf: u32, size: u32) !u32 {
-    const data: [*]u8 = @ptrFromInt(buf);
+pub fn write(fd: u32, buf: ?[*]u8, size: u32) !u32 {
     if (krn.task.current.files.fds.get(fd)) |file| {
         file.ref.ref();
         defer file.ref.unref();
         if (!file.canWrite())
-            return errors.PosixError.EACCES;
+            return errors.EACCES;
+        if (size == 0)
+            return 0;
+        const data = buf orelse
+            return errors.EFAULT;
         if (file.flags & krn.fs.file.O_APPEND != 0) {
             file.pos = file.inode.size;
         }
         return try file.ops.write(file, data, size);
     }
     krn.logger.INFO("Error {d}\n", .{krn.task.current.pid});
-    return errors.PosixError.EBADF;
+    return errors.EBADF;
 }
 
 pub const IoVec = extern struct {
-    base: [*]u8,
+    base: ?[*]u8,
     len: usize,
 };
 
-pub fn writev(fd: u32, iov: [*]IoVec, iovcnt: u32) !u32 {
+pub fn writev(fd: u32, iov: ?[*]IoVec, iovcnt: u32) !u32 {
+    if (iovcnt == 0)
+        return 0;
+    const iovec = iov orelse
+        return errors.EFAULT;
     var ret: u32 = 0;
     for (0..iovcnt) |idx| {
-        const curr = iov[idx];
+        const curr = iovec[idx];
         if (curr.len > 0) {
-            const single_write: u32 = try write(
-                fd,
-                @intFromPtr(curr.base),
-                curr.len
-            );
+            const single_write: u32 = try write(fd, curr.base, curr.len);
             ret += single_write;
             if (single_write != curr.len) {
                 return ret;
@@ -43,7 +46,7 @@ pub fn writev(fd: u32, iov: [*]IoVec, iovcnt: u32) !u32 {
     return ret;
 }
 
-pub fn pwritev(fd: u32, iov: [*]IoVec, iovcnt: u32, offset: u32) !u32 {
+pub fn pwritev(fd: u32, iov: ?[*]IoVec, iovcnt: u32, offset: u32) !u32 {
     _ = offset;
     return try writev(fd, iov, iovcnt);
 }
